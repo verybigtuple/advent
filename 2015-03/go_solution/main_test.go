837cdbb212488d00cb0c31e1c977b2ca7d3189b4
verybigtuple <bigtuple@gmail.com>
2024-09-12 00:07:24 +0100

    fixed tests

--- 2015-03/go_solution/main_test.go
+++ 2015-03/go_solution/main_test.go
@@ -8,23 +8,27 @@ import (
 func TestProcess(t *testing.T) {
 	testCases := []struct {
 		input string
-		want  int
+		wantSanta  int
+		wantRobo int
 	}{
-		{"", 1},
-		{">", 2},
-		{"^>v<", 4},
-		{"^v^v^v^v^v", 2},
+		{"", 1, 1},
+		{">", 2, 2},
+		{"^>v<", 4, 3},
+		{"^v^v^v^v^v", 2, 11},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.input, func(t *testing.T) {
 			reader := strings.NewReader(tc.input)
-			got, err := process(reader)
+			gotSanta, gotRobo, err := Process(reader)
 			if err != nil {
 				t.Errorf("Error during process: %v", err)
 			}
-			if got != tc.want {
-				t.Errorf("Want: %d, got: %d", tc.want, got)
+			if gotSanta != tc.wantSanta {
+				t.Errorf("Want Santa: %d, got Santa: %d", tc.wantSanta, gotSanta)
+			}
+			if gotRobo != tc.wantRobo {
+				t.Errorf("Want Robo: %d, got Robo: %d", tc.wantRobo, gotRobo)
 			}
 		})
 	}
