fa556ca81e99d9ecbb33ce90f673bdfa5b8ca25e
verybigtuple <bigtuple@gmail.com>
2024-10-23 02:00:19 +0100

    Added first draft of algo for nice 2

--- 2015-05/go-solution/main.go
+++ 2015-05/go-solution/main.go
@@ -26,6 +26,53 @@ func IsNice(input string) bool {
 	return vowels >= 3 && pairs >= 1
 }
 
+type Pair struct {
+	R1 rune
+	R2 rune
+}
+
+func IsNice2(input string) bool {
+	pairs := make(map[Pair]int)
+	var hasPairs, hasDouble bool
+	var prev, prevPrev rune
+	for i, cur := range input {
+		if !hasPairs {
+			if i == 0 {
+				prev = cur
+				continue
+			}
+
+			currentPair := Pair{prev, cur}
+			if ind, ok := pairs[currentPair]; ok && (i-ind) > 1 {
+				hasPairs = true
+			} else {
+				pairs[currentPair] = i
+			}
+		}
+
+		if !hasDouble {
+			if i == 0 {
+				prev = cur
+				continue
+			}
+			if i == 1 {
+				prevPrev = prev
+				prev = cur
+				continue
+			}
+
+			hasDouble = prevPrev == cur
+		}
+
+		if hasPairs && hasDouble {
+			return true
+		}
+		prevPrev = prev
+		prev = cur
+	}
+	return false
+}
+
 func ReadFile() error {
 	file, err := os.Open("input.txt")
 	if err != nil {
--- 2015-05/go-solution/main_test.go
+++ 2015-05/go-solution/main_test.go
@@ -23,3 +23,25 @@ func TestIsNice(t *testing.T) {
 		})
 	}
 }
+
+func TestIsNice2(t *testing.T) {
+	tests := []struct {
+		input string
+		want  bool
+	}{
+		{"qjhvhtzxzqqjkmpb", true},
+		{"xxyxx", true},
+		{"aaa", false},
+		{"uurcxstgmygtbstg", false},
+		{"ieodomkazucvgmuy", false},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.input, func(t *testing.T) {
+			got := IsNice2(tc.input)
+			if tc.want != got {
+				t.Errorf("want: %v, got: %v", tc.want, got)
+			}
+		})
+	}
+}
