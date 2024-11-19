49c3fbae9be3884232ac0c42227dc2c9446fb0ac
verybigtuple <bigtuple@gmail.com>
2024-11-19 22:48:45 +0000

    Added range

---
+++ 2015-06/rust_solution/src/range.rs
@@ -0,0 +1,102 @@
+const MAX_X: usize = 999;
+const MAX_Y: usize = 999;
+
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+pub struct Point(pub usize, pub usize);
+
+#[derive(Clone, Copy, Debug)]
+pub struct PointRange {
+    from: Point,
+    to: Point,
+    max: Point,
+    x: usize,
+    y: usize,
+}
+
+impl PointRange {
+    pub fn new(from: Point, to: Point) -> PointRange {
+        Self::with_max(from, to, Point(MAX_X, MAX_Y))
+    }
+
+    pub fn with_max(from: Point, to: Point, max: Point) -> PointRange {
+        if from > to {
+            panic!("To point should be more than From point");
+        }
+
+        if from > max || to > max {
+            panic!("points cannot be more than max");
+        }
+
+        PointRange {
+            from,
+            to,
+            max,
+            x: from.0,
+            y: from.1,
+        }
+    }
+}
+
+impl Iterator for PointRange {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.y > self.to.1 {
+            return None;
+        }
+
+        let current = self.x + self.y * (self.max.0 + 1);
+
+        if self.x < self.to.0 {
+            self.x += 1;
+        } else {
+            self.x = self.from.0;
+            self.y += 1;
+        }
+
+        Some(current)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty_range() {
+        let start = Point(0, 0);
+        let mut r = PointRange::with_max(start, start, start);
+        assert_eq!(Some(0), r.next());
+        assert_eq!(None, r.next());
+    }
+
+    #[test]
+    fn test_one_point() {
+        let mut r = PointRange::with_max(Point(1, 1), Point(1, 1), Point(2, 2));
+        assert_eq!(Some(4), r.next());
+        assert_eq!(None, r.next());
+    }
+
+    #[test]
+    fn test_range() {
+        let mut r = PointRange::with_max(Point(1, 1), Point(2, 2), Point(2, 2));
+        for i in (4..=8).map(|x| x as usize) {
+            assert_eq!(Some(i), r.next());
+        }
+        assert_eq!(None, r.next());
+    }
+
+    #[test]
+    fn test_count_big_range() {
+        let mut r = PointRange::with_max(Point(0, 0), Point(999, 999), Point(999, 999));
+        let mut count = r.count();
+        assert_eq!(1_000_000, count);
+    }
+
+    #[test]
+    fn test_count_null_range() {
+        let mut r = PointRange::with_max(Point(499, 499), Point(500, 500), Point(999, 999));
+        let mut count = r.count();
+        assert_eq!(4, count);
+    }
+}
