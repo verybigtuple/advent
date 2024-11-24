f9c2026cb4d6456cc6f730d07279decb0997790c
verybigtuple <bigtuple@gmail.com>
2024-11-24 23:43:38 +0000

    Added solution for the second part of problem 2015-06

--- 2015-06/rust_solution/src/main.rs
+++ 2015-06/rust_solution/src/main.rs
@@ -2,16 +2,13 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 mod bitmap;
-mod range;
+mod bytemap;
 mod parser;
+mod range;
 
-
-fn do_instruction(bitmap: &mut bitmap::BitMap, intruct: &str) {
-    let mut paser = parser::Parser::new(intruct);
-    let command = paser.parse().unwrap();
-    let range = range::PointRange::new(command.from, command.to);
+fn do_onoff(bitmap: &mut bitmap::BitMap, range: range::PointRange, operation: parser::Operation) {
     for p in range {
-        match command.op {
+        match operation {
             parser::Operation::Toggle => bitmap.toggle_bit(p),
             parser::Operation::TurnOff => bitmap.reset_bit(p),
             parser::Operation::TurnOn => bitmap.set_bit(p),
@@ -19,27 +16,33 @@ fn do_instruction(bitmap: &mut bitmap::BitMap, intruct: &str) {
     }
 }
 
-fn main() {
-    let mut bm = bitmap::BitMap::new(1_000_000);
-    let file = File::open("input.txt").unwrap();
-    let reader = BufReader::new(file);
-
-    for line in reader.lines() {
-        do_instruction(&mut bm, line.unwrap().as_str());
+fn do_brightness(
+    brightness: &mut bytemap::ByteMap,
+    range: range::PointRange,
+    operation: parser::Operation,
+) {
+    for p in range {
+        match operation {
+            parser::Operation::Toggle => brightness.inc_byte_by(p, 2),
+            parser::Operation::TurnOff => brightness.dec_byte(p),
+            parser::Operation::TurnOn => brightness.inc_byte(p),
+        }
     }
-    println!("{}", bm.count())
 }
 
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test1 () {
-        let a = "toggle 0,0 through 999,0";
-        let mut bm = bitmap::BitMap::new(1_000_000);
-        do_instruction(&mut bm, a);
-        assert_eq!(1_000, bm.count());
+fn main() {
+    let mut bm = bitmap::BitMap::new(1_000_000);
+    let mut brightness = bytemap::ByteMap::new();
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let mut parser = parser::Parser::new(&line);
+        let command = parser.parse().unwrap();
+        let range = range::PointRange::new(command.from, command.to);
+        do_onoff(&mut bm, range, command.op);
+        do_brightness(&mut brightness, range, command.op);
     }
+    println!("first part: {}", bm.count());
+    println!("second part: {}", brightness.count());
 }
